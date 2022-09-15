1d0d307e3f127d63af182512398bc175ca39bace
earth <earthicko@gmail.com>
2022-09-16 07:49:41 +0900

    add stack

---
+++ stack.c
@@ -0,0 +1,48 @@
+#include "stack.h"
+
+int	stack_swap(t_deque *this)
+{
+	t_node	*node0;
+	t_node	*node1;
+
+	if (this->size < 2)
+		return (CODE_ERROR_STACK_TOO_SMALL);
+	node0 = deque_pop_head(this);
+	node1 = deque_pop_head(this);
+	deque_push_head(this, node0);
+	deque_push_head(this, node1);
+	return (CODE_OK);
+}
+
+int	stack_push(t_deque *from, t_deque *to)
+{
+	t_node	*node;
+
+	if (from->size < 1)
+		return (CODE_ERROR_STACK_TOO_SMALL);
+	node = deque_pop_head(from);
+	deque_push_head(to, node);
+	return (CODE_OK);
+}
+
+int	stack_rotate(t_deque *this)
+{
+	t_node	*node;
+
+	if (this->size < 2)
+		return (CODE_ERROR_STACK_TOO_SMALL);
+	node = deque_pop_head(this);
+	deque_push_tail(this, node);
+	return (CODE_OK);
+}
+
+int	stack_rev_rotate(t_deque *this)
+{
+	t_node	*node;
+
+	if (this->size < 2)
+		return (CODE_ERROR_STACK_TOO_SMALL);
+	node = deque_pop_tail(this);
+	deque_push_head(this, node);
+	return (CODE_OK);
+}
---
+++ stack.h
@@ -0,0 +1,4 @@
+#ifndef STACK_H
+# define STACK_H
+# include "t_deque.h"
+#endif
