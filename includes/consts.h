12210a5015e14e81fb37fa3ad5767170d531512d
earthicko <earthicko@gmail.com>
2022-09-16 10:26:44 +0900

    update consts

--- includes/consts.h
+++ includes/consts.h
@@ -2,5 +2,7 @@
 # define CONSTS_H
 # define CODE_OK 0
 # define CODE_ERROR_GENERIC -1
-# define CODE_ERROR_STACK_TOO_SMALL -2
+# define CODE_ERROR_INVALID_VALUE -2
+# define CODE_ERROR_MALLOC -3
+# define CODE_ERROR_STACK_TOO_SMALL -100
 #endif
