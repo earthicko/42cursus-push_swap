be198274e9f9338a126b0500aacf8c4eea686f1f
earthicko <earthicko@gmail.com>
2022-09-16 17:23:42 +0900

    직접 정렬 로직 추가

--- includes/sorter.h
+++ includes/sorter.h
@@ -5,4 +5,6 @@
 int	sort_stack(t_state *state);
 int	sort_a_to_b(t_state *state, int s, int e);
 int	sort_b_to_a(t_state *state, int s, int e);
+int	sort_two_a(t_state *state);
+int	sort_two_b(t_state *state);
 #endif
---
+++ sorter/sort_manual.c
@@ -0,0 +1,21 @@
+#include "t_state.h"
+
+int	sort_two_a(t_state *state)
+{
+	if (state->a->size < 2)
+		return (CODE_ERROR_INVALID_VALUE);
+	if (state->a->head->idx > state->a->head->next->idx)
+		sa(state);
+	return (CODE_OK);
+}
+
+int	sort_two_b(t_state *state)
+{
+	if (state->b->size < 2)
+		return (CODE_ERROR_INVALID_VALUE);
+	if (state->b->head->idx < state->b->head->next->idx)
+		sb(state);
+	pa(state);
+	pa(state);
+	return (CODE_OK);
+}
