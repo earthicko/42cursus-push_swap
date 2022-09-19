eb2d911355e9f533d1d1018a8ec010516d89c9f0
earthicko <earthicko@gmail.com>
2022-09-20 00:50:04 +0900

    state debug 디버그

--- debug/state_debug.c
+++ debug/state_debug.c
@@ -13,20 +13,20 @@
 #include <stdio.h>
 #include "state_debug.h"
 
-void	print_one_stack_layer(t_state *state, int i_line, t_node *a, t_node *b)
+void	print_one_stack_layer(t_state *state, int i_line, t_node **a, t_node **b)
 {
 	if (i_line < state->a->size)
 	{
-		printf("%2d", a->idx);
-		a = a->next;
+		printf("%2d", (*a)->idx);
+		*a = (*a)->next;
 	}
 	else
 		printf("  ");
 	printf(" | ");
 	if (i_line < state->b->size)
 	{
-		printf("%2d", b->idx);
-		b = b->next;
+		printf("%2d", (*b)->idx);
+		*b = (*b)->next;
 	}
 	else
 		printf("  ");
@@ -43,13 +43,14 @@ void	print_state(t_state *state)
 	max_len = state->a->size;
 	if (state->b->size > max_len)
 		max_len = state->b->size;
+	printf("a: [%d], b: [%d]\n", state->a->size, state->b->size);
 	printf("-------\n");
 	a = state->a->head;
 	b = state->b->head;
 	i = 0;
 	while (i < max_len)
 	{
-		print_one_stack_layer(state, i, a, b);
+		print_one_stack_layer(state, i, &a, &b);
 		i++;
 	}
 	printf("-------\n");
